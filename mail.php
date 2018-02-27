33607f6695d744dd5d907dbc4fe837ed9df3103c
nachodev <neosorro@gmail.com>
2018-02-27 19:26:24 +0100

    adding social networks and new mentors

--- mail.php
+++ mail.php
@@ -1,15 +1,17 @@
 <?php
-$mail = "neosorro@gmail.com";
+$mail = "vocacion@yourbestyou.es";
 $title = "[YourBestYou] Solicitud de mentoría";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // El mensaje
-$message = "Ha habido una nueva solicitud de mentoría para " .$_POST['mentor'] . "<br>";
-$message .= "Nombre: " . $_POST['name'] . "<br>";
-$message .= "Email: " . $_POST['email'] . "<br>" ;
-$message .= "Mensaje: " . $_POST['message'] . "<br>";
+$message = "Ha habido una nueva solicitud de mentoría para " .$_POST['mentor'] . "<br><br>";
+$message .= "<strong> Nombre: </strong> " . $_POST['name'] . "<br><br>";
+$message .= "<strong> Email: </strong>" . $_POST['email'] . "<br><br>" ;
+$message .= "<strong> Mensaje: </strong>" . $_POST['message'] . "<br><br>";
 
 // Enviarlo
-mail( $mail, $title, $message);
+mail( $mail, $title, $message, $headers);
 
 echo json_encode($_POST);
 ?>
